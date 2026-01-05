632e78a0fba790b1e990090b80cee6ca17dfb0de
harshalmoraskar14 <harshalmoraskar14@gmail.com>
2026-01-05 13:45:37 +0530

    first

---
+++ laptop_price/exception.py
@@ -0,0 +1,9 @@
+class PricePredictorException(Exception):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+        
+        
+        
+        
+        
